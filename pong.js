a960c541fe41c06e1df8f2ad3835013c9f25409d
darwinchiu <darwinchiu127@gmail.com>
2016-03-08 22:34:23 -0500

    create paddles for players

--- pong.js
+++ pong.js
@@ -28,3 +28,43 @@ var render = function() {
   ctx.fillStyle = "#7F7F7F";
   ctx.fillRect(0, 0, width, height);
 };
+
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.x_speed = 0;
+  this.y_speed = 0;
+}
+
+Paddle.prototype.render = function() {
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+};
+
+function Player() {
+   this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+function Computer() {
+  this.paddle = new Paddle(175, 10, 50, 10);
+}
+
+Player.prototype.render = function() {
+  this.paddle.render();
+};
+
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
+
+var player = new Player();
+var computer = new Computer();
+
+var render = function() {
+  ctx.fillStyle = "#7F7F7F";
+  ctx.fillRect(0, 0, width, height);
+  player.render();
+  computer.render();
+};
